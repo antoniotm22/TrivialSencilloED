de880e89f2e0549ef8483878c5f4e2d7a040c3d8
Antonio Jesús Trujillo <55539288+antoniotm22@users.noreply.github.com>
2020-11-01 23:10:43 +0100

    novena pregunta

--- src/Juego.java
+++ src/Juego.java
@@ -273,8 +273,44 @@ public class Juego {
 		}
 		
 		System.out.println("Tú número de aciertos es: " + numAciertos + " , mucho ánimo");
+		
+		//Novena pregunta
+		
+		String pregunta9 = JOptionPane.showInputDialog("¿Qué ciudad presume de haber tenido el primer metro del mundo?");
+		String respuesta9 = "londres";
+		if(pregunta9.equals(respuesta9)) {
+			numAciertos ++;
+			System.out.println("\r\n" + 
+					"     ___       ______  __   _______ .______     .___________.  ______   \r\n" + 
+					"    /   \\     /      ||  | |   ____||   _  \\    |           | /  __  \\  \r\n" + 
+					"   /  ^  \\   |  ,----'|  | |  |__   |  |_)  |   `---|  |----`|  |  |  | \r\n" + 
+					"  /  /_\\  \\  |  |     |  | |   __|  |      /        |  |     |  |  |  | \r\n" + 
+					" /  _____  \\ |  `----.|  | |  |____ |  |\\  \\----.   |  |     |  `--'  | \r\n" + 
+					"/__/     \\__\\ \\______||__| |_______|| _| `._____|   |__|      \\______/  \r\n" + 
+					"                                                                        \r\n" + 
+					"");
+			
+		}else {
+			System.out.println("\r\n" + 
+					" _______    ___       __       __        ______   \r\n" + 
+					"|   ____|  /   \\     |  |     |  |      /  __  \\  \r\n" + 
+					"|  |__    /  ^  \\    |  |     |  |     |  |  |  | \r\n" + 
+					"|   __|  /  /_\\  \\   |  |     |  |     |  |  |  | \r\n" + 
+					"|  |    /  _____  \\  |  `----.|  `----.|  `--'  | \r\n" + 
+					"|__|   /__/     \\__\\ |_______||_______| \\______/  \r\n" + 
+					"                                                  \r\n" + 
+					"");
+			
+		}
+		
+		System.out.println("Tú número de aciertos es: " + numAciertos + " , mucho ánimo");
+		
+		
+		
 	}
 
+	
+	
 	
 	
 	
