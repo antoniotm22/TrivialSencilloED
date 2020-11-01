a2a365dadac8c3f0929d4565d218174999cfa6ce
Antonio Jesús Trujillo <55539288+antoniotm22@users.noreply.github.com>
2020-11-01 23:03:23 +0100

    Séptima pregunta

--- src/Juego.java
+++ src/Juego.java
@@ -210,6 +210,38 @@ public class Juego {
 		
 		System.out.println("Tú número de aciertos es: " + numAciertos + " , mucho ánimo");
 		
+		
+		//Séptima pregunta
+		
+		String pregunta7 = JOptionPane.showInputDialog("¿En qué ciudad se hizo la primera llamada desde un telefono móvil?");
+		String respuesta7 = "nueva york";
+		if(pregunta7.equals(respuesta7)) {
+			numAciertos ++;
+			System.out.println("\r\n" + 
+					"     ___       ______  __   _______ .______     .___________.  ______   \r\n" + 
+					"    /   \\     /      ||  | |   ____||   _  \\    |           | /  __  \\  \r\n" + 
+					"   /  ^  \\   |  ,----'|  | |  |__   |  |_)  |   `---|  |----`|  |  |  | \r\n" + 
+					"  /  /_\\  \\  |  |     |  | |   __|  |      /        |  |     |  |  |  | \r\n" + 
+					" /  _____  \\ |  `----.|  | |  |____ |  |\\  \\----.   |  |     |  `--'  | \r\n" + 
+					"/__/     \\__\\ \\______||__| |_______|| _| `._____|   |__|      \\______/  \r\n" + 
+					"                                                                        \r\n" + 
+					"");
+			
+		}else {
+			System.out.println("\r\n" + 
+					" _______    ___       __       __        ______   \r\n" + 
+					"|   ____|  /   \\     |  |     |  |      /  __  \\  \r\n" + 
+					"|  |__    /  ^  \\    |  |     |  |     |  |  |  | \r\n" + 
+					"|   __|  /  /_\\  \\   |  |     |  |     |  |  |  | \r\n" + 
+					"|  |    /  _____  \\  |  `----.|  `----.|  `--'  | \r\n" + 
+					"|__|   /__/     \\__\\ |_______||_______| \\______/  \r\n" + 
+					"                                                  \r\n" + 
+					"");
+			
+		}
+		
+		System.out.println("Tú número de aciertos es: " + numAciertos + " , mucho ánimo");
+		
 	}
 
 	
